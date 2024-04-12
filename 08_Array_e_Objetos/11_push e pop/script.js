9a65991fec886303f8b50b8f3df50e1d015820ee
Jorge Targino <109600625+JorgeTargino@users.noreply.github.com>
2024-04-12 09:35:43 -0300

    Delete 08_Array_e_Objetos directory

--- 08_Array_e_Objetos/11_push e pop/script.js
+++
@@ -1,9 +0,0 @@
-let nomes = ["Tomé", "Madalena", "Judas"];
-
-let elementoRemovido = nomes.pop();//Remove um item ao final do Array
-
-console.log(elementoRemovido);
-console.log(nomes);
-
-nomes.push('Pedro');//Adicionar um item ao final do Array
-console.log(nomes);
