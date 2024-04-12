ec5cfd10fee972e42a7ab64139ac1c29f880a466
Jorge Targino <109600625+JorgeTargino@users.noreply.github.com>
2024-04-12 09:35:21 -0300

    Delete 06_Funçoes directory

--- 06_Funçoes/9_args_default/script.js
+++
@@ -1,7 +0,0 @@
-function potencial(base, exp=2) {
-     return Math.pow(base,exp);
-}
-
-console.log(potencial(2));// O argumento já foi pre-determinado pelo "exp=2"
-console.log(potencial(2,2));
-console.log(potencial(2,3));
