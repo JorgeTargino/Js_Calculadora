0f4cc318849664ef4527934efd2bbced421849f6
Jorge Targino <109600625+JorgeTargino@users.noreply.github.com>
2024-04-12 09:36:29 -0300

    Delete 16_Javascript_e_o_Navegador directory

--- 16_Javascript_e_o_Navegador/06_by_class/js/scripts.js
+++
@@ -1,7 +0,0 @@
-console.log(document.getElementsByTagName('h1'));
-
-console.log(document.getElementsByClassName('itens-azuis'));
-
-console.log(document.getElementsByClassName('itens-vermelhos'));
-
-console.log(document.getElementsByClassName('itens'));
