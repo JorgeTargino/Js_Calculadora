e88e2a75791fc31101c33391765bc6b2cfd51607
Jorge Targino <109600625+JorgeTargino@users.noreply.github.com>
2024-04-12 09:35:36 -0300

    Delete 09_Exercicios_arrays_e_obj directory

--- 09_Exercicios_arrays_e_obj/02_exercicio_27/script.js
+++
@@ -1,5 +0,0 @@
-let pessoa1 = ["Jheny", "7 anos"];
-let pessoa2 = ["Bernardo", "12 anos", "cagão", "jogador"]
-
-console.log(pessoa1.length);
-console.log(pessoa2.length);
--- 09_Exercicios_arrays_e_obj/03_exercicio_28/script.js
+++
@@ -1,9 +0,0 @@
-let onibus = {
-    rodas: 8,
-    limite_pas: 40,
-    portas: 2
-}
-
-console.log(onibus.rodas);
-console.log(onibus.limite_pas);
-console.log(onibus.portas);
